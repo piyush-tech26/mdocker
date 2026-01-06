6f316042546ff60e50eca9d5f557dc8f231daae9
piyush-tech26 <piyushbaid20120943@gmail.com>
2026-01-06 11:15:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -300,6 +300,10 @@ void cleanup_cgroup(const char *cgroup_path) {
 }
 
 int remove_callback(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+    (void)sb;
+    (void)typeflag;
+    (void)ftwbuf;
+    
     int rv = remove(fpath);
     if (rv) perror(fpath);
     return rv;
